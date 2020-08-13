89185df7b2f245715d84f969dbeea629579d4655
Hanzel <hgodinez89@hotmail.com>
2020-08-13 16:07:44 -0600

    Upgrade some packages as Django 3.0.1 => 3.1, Pillow 6.2.1 => 7.1.0; and uninistalling of package as python-http-client and sendgrid

--- store/__init__.py
+++ store/__init__.py
@@ -1,8 +1,7 @@
-# Este archivo nos permite convertir la carpeta en un modulo
 import pymysql
 
 # This line is neccesary to avoid the error:
-# mysqlclient 1.3.13 or newer is required; you have 0.9.3
-pymysql.version_info = (1, 3, 13, "final", 0)
+# mysqlclient 1.4.0 or newer is required; you have 0.9.3
+pymysql.version_info = (1, 4, 0, "final", 0)
 
 pymysql.install_as_MySQLdb()
